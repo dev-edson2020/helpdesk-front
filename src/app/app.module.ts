48edbd9f6dd33e05a3666a7bb3fb87177e59954b
Edson Meira <edsonanga@hotmail.com>
2024-02-27 17:17:50 -0300

    Criação dos componentes do sistema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { MatRadioModule } from '@angular/material/radio';
@@ -25,53 +26,50 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { NavComponent } from './components/nav/nav.component';
 
 // Componentes do projeto
-// import { NavComponent } from './components/nav/nav.component';
-// import { HomeComponent } from './components/home/home.component';
-// import { HeaderComponent } from './components/header/header.component';
-// import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
-// import { LoginComponent } from './components/login/login.component';
-// import { ToastrModule } from 'ngx-toastr';
-// import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
-// import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
-// import { NgxMaskModule } from 'ngx-mask';
-// import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
-// import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
-// import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
-// import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
-// import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
-// import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
-// import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
-// import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
-// import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
-// import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-read.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './components/login/login.component';
+import { ToastrModule } from 'ngx-toastr';
+import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
+import { NgxMaskModule } from 'ngx-mask';
+import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
+import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
+import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
+import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
+import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
+import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
+import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-read.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    // NavComponent,
-    // HomeComponent,
-    // HeaderComponent,
-    // TecnicoListComponent,
-    // LoginComponent,
-    // TecnicoCreateComponent,
-    // TecnicoUpdateComponent,
-    // TecnicoDeleteComponent,
-    // ClienteCreateComponent,
-    // ClienteDeleteComponent,
-    // ClienteListComponent,
-    // ClienteUpdateComponent,
-    // ChamadoListComponent,
-    // ChamadoCreateComponent,
-    // ChamadoUpdateComponent,
-    // ChamadoReadComponent
+    HomeComponent,
+    HeaderComponent,
+    TecnicoListComponent,
+    LoginComponent,
+    TecnicoCreateComponent,
+    TecnicoUpdateComponent,
+    TecnicoDeleteComponent,
+    ClienteCreateComponent,
+    ClienteDeleteComponent,
+    ClienteListComponent,
+    ClienteUpdateComponent,
+    ChamadoListComponent,
+    ChamadoCreateComponent,
+    ChamadoUpdateComponent,
+    ChamadoReadComponent
   ],
   imports: [
-  BrowserModule,
+    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     // Forms
@@ -94,14 +92,14 @@ import { NavComponent } from './components/nav/nav.component';
     MatIconModule,
     MatListModule,
     MatCardModule,
-    // ToastrModule.forRoot({
-    //   timeOut: 4000,
-    //   closeButton: true,
-    //   progressBar: true
-    // }),
-    // NgxMaskModule.forRoot()
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      closeButton: true,
+      progressBar: true
+    }),
+    NgxMaskModule.forRoot()
   ],
-  // providers: [AuthInterceptorProvider],
+  providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
